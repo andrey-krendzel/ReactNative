558bf68ff8b8ea9a80b4dde9832bb8349da67830
user <andreykrendzel@hotmail.com>
2020-07-05 12:51:03 +0300

    Redux Adding Favourites

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
+import React, { Component } from 'react';
+import Main from './components/MainComponent.js';
 
 const store = ConfigureStore();
 
